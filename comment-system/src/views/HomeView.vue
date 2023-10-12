8c230c360b68c0052456cc995b2e25a341a65f1b
SeaklongRa <raseaklongkh@gmail.com>
2023-10-12 22:45:18 +0700

    commit

--- comment-system/src/views/HomeView.vue
+++ comment-system/src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <template>
-  <div><h1>Hello</h1></div>
+  <v-container>
+    <h1>Hello</h1>
+    <comment-input />
+  </v-container>
 </template>
 
 <script>
+import CommentInput from '../components/comments/comment-input.vue'
 
   export default {
     name: 'HomePage',
 
     components: {
+      CommentInput
     },
   }
 </script>
