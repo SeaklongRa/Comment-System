404c6ce30d6c0e97cda352e20749f07a2772316f
SeaklongRa <raseaklongkh@gmail.com>
2023-10-12 22:17:50 +0700

    clean file

--- comment-system/src/views/HomeView.vue
+++ comment-system/src/views/HomeView.vue
@@ -1,15 +1,13 @@
 <template>
-  <hello-world />
+  <div><h1>Hello</h1></div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'HomePage',
 
     components: {
-      HelloWorld,
     },
   }
 </script>
